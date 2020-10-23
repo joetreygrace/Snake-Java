b370c2289b477600cecd8b0356fc123bbea94574
Joe Grace <joeggrace@gmail.com>
2020-10-23 09:59:44 -0400

    Added key Listener

--- src/game/treygrace/SnakeGame.java
+++ src/game/treygrace/SnakeGame.java
@@ -2,8 +2,10 @@ package game.treygrace;
 
 import java.applet.Applet;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class SnakeGame extends Applet implements Runnable{
+public class SnakeGame extends Applet implements Runnable, KeyListener {
     public void init(){
 
     }
@@ -25,6 +27,21 @@ public class SnakeGame extends Applet implements Runnable{
 
     @Override
     public void run() {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
 
     }
 }
