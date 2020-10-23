c6b94187dccd490d3966c2ce82f78923d48651b4
Joe Grace <joeggrace@gmail.com>
2020-10-23 09:58:34 -0400

    Added Game class

---
+++ src/game/treygrace/SnakeGame.java
@@ -0,0 +1,30 @@
+package game.treygrace;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class SnakeGame extends Applet implements Runnable{
+    public void init(){
+
+    }
+
+    @Override
+    public void paint(Graphics g) {
+
+    }
+
+    @Override
+    public void repaint() {
+
+    }
+
+    @Override
+    public void update(Graphics g) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
